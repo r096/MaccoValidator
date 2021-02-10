03237d0ae2a29690f3f1a3061c0f1ba2b67077dd
rahul singh <rahulsingh@rahuls-MacBook-Pro.local>
2021-02-10 21:57:50 +0530

    rahul

---
+++ MaccoValidator/MaccoValidator/MaccoValidator.h
@@ -0,0 +1,18 @@
+//
+//  MaccoValidator.h
+//  MaccoValidator
+//
+//  Created by rahul singh on 10/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaccoValidator.
+FOUNDATION_EXPORT double MaccoValidatorVersionNumber;
+
+//! Project version string for MaccoValidator.
+FOUNDATION_EXPORT const unsigned char MaccoValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaccoValidator/PublicHeader.h>
+
+
